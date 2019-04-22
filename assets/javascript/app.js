bfa5de0d720eb7ed52c98c67af0ea08789baf191
Stephanie Sapp <stephs1531@gmail.com>
2019-04-22 09:11:59 -0500

    psuedocode js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//set variables for each form input
+
+//initialize firebase
+//get reference to database
+
+//write function for .on("click") of submit button
+
+    //get the inputs from form
+    //create an object for newTrain with data gathered from input
+    //.push(newTrain) to firebase.database().ref()
+
+    //set function so that when it is hit, it will grab every single child from database and listen for any new ones you add
+
+        //set variables to be the child.val() of each data point
+        //add new data to a new table row
+        //append new row of data to htl
+    
